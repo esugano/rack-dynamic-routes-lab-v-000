842982335e41052d7f912372ce5f78f87a4e6f44
Emily S <emily.sugano@gmail.com>
2018-06-27 16:08:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,13 +6,12 @@ class Application
     resp =Rack::Response.new
     req = Rack::Request.new(env)
     #if /items/<ITEM NAME> route
-          binding.pry
+    binding.pry
     if req.path=="/items"
       #get item name
       item_name = req.path.split("/items/").last
       #see if item exists
       item_find = Item.all.find{|i| i.name == item_name}
-      binding.pry
       #if items exists, return its price
       if req.path.match("/items/#{item_name}")
       #if item doesn't exists, return a 400 page
