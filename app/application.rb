d093d8bb485c14192ffde3cfbd29c01792aabfc4
Emily S <emily.sugano@gmail.com>
2018-06-27 15:58:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
 
   def call(env)
     resp =Rack::Response.new
-    req = Rack::Response.new(env)
+    req = Rack::Request.new(env)
     binding.pry
     #if /items/<ITEM NAME> route
     if req.path.match("/items/")
